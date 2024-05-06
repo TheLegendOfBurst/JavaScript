add059750c43c5d59dadb24d77328b557d361fe8
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-06 15:44:40 -0300

    Create concatenar.js

---
+++ concatenar.js
@@ -0,0 +1,15 @@
+/*
+var dono = 'Davi';
+var ddd = '(61)';
+var telefone = '9 9999-9999';
+var numero = ddd.concat(" " + telefone);
+console.log ("O Cliente", dono, "tem o seguinte numero de celular",ddd, telefone);
+console.log ("O Cliente "+ dono + " tem o seguinte numero de celular "+ numero);
+
+let texto = `O Cliente ${dono} tem o seguinte numero de celular ${ddd} ${telefone}`
+console.log(texto)
+*/
+
+
+var idade = 21
+console.log(idade + 1);
