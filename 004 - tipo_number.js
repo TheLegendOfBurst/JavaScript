93b4aab738f6a4d32e1d79105250de053a3e77dd
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-06 17:32:24 -0300

    ok

--- 004 - tipo_number.js
+++ 004 - tipo_number.js
@@ -36,3 +36,14 @@ console.log(typeof number, number);
 var number = new Date();
 number.setHours(number.getHours() - (number.getTimezoneOffset() / 60));
 console.log(typeof number, number);
+
+var number = Number(new Date());
+console.log(typeof number, number);
+
+var n = '29.230421598321684'
+console.log(typeof n, n)
+console.log(typeof parseInt(n), parseInt(n))
+
+var n = '29.23'
+console.log(typeof n, n)
+console.log(typeof parseFloat(n), parseFloat(n))
---
+++ 005 - operadores_aritmeticos.js
@@ -0,0 +1,49 @@
+/*
+TIPOS
+    Adição(+)
+    Subtração(-)
+    Divisão(/)
+    Multiplicação(*)
+    Resto da Divisão(%)
+    Incremento(++)
+    Descremento(--)
+    Expoente(**)
+*/
+
+let v1 = 2;
+let v2 = 3;
+let soma = v1+v2;
+console.log("A soma é:"+soma)
+console.log("A soma é:",v1+v2)
+console.log("A soma é:"+parseInt(v1+v2))
+console.log("A soma é:"+Number(v1+v2))
+
+//SUBTRAÇÃO
+
+let v3 = 9;
+let v4 = 4;
+let subtracao = v3+v4;
+console.log("A subtração é:"+subtracao)
+console.log("A subtração é:",v3+v4)
+console.log("A subtração é:"+parseInt(v3+v4))
+console.log("A subtração é:"+Number(v3+v4))
+
+//DIVISÃO
+
+let v5 = 10;
+let v6 = 15;
+let divisao = v5+v6;
+console.log("A divisão é:"+divisao)
+console.log("A divisão é:",v5+v6)
+console.log("A divisão é:"+parseInt(v5+v6))
+console.log("A divisão é:"+Number(v5+v6))
+
+//MULTIPLICAÇÃO
+
+let v7 = 10;
+let v8 = 15;
+let multiplicacao = v7+v8;
+console.log("A multiplicação é:"+multiplicacao)
+console.log("A multiplicação é:",v7+v8)
+console.log("A multiplicação é:"+parseInt(v7+v8))
+console.log("A multiplicação é:"+Number(v7+v8))
