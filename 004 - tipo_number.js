ec72ad3f833e96dafd7980e75c79356405526906
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-06 16:42:12 -0300

    Update 004 - tipo_number.js

--- 004 - tipo_number.js
+++ 004 - tipo_number.js
@@ -8,5 +8,31 @@ TIPO NUMERO
 
 */
 
-let idade = 21;
+var idade = 21;
 console.log(typeof idade, idade);
+console.log(typeof idade, idade+1);
+
+
+var idade = '21';
+console.log(typeof idade, idade);
+console.log(typeof idade, idade+1);
+
+
+var idade = Number('21');
+console.log(typeof idade, idade);
+console.log(typeof idade, idade+1);
+
+
+var number = true;
+console.log(typeof number, number);
+
+
+var number = Number(false);
+console.log(typeof number, number);
+var number = Number(true);
+console.log(typeof number, number);
+
+
+var number = new Date();
+number.setHours(number.getHours() - (number.getTimezoneOffset() / 60));
+console.log(typeof number, number);
