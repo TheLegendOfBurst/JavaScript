301c2406fb95117cdf8ad2b0e20b16c5f7d81613
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-06 15:01:48 -0300

    Create tipo_primitivo.js

---
+++ tipo_primitivo.js
@@ -0,0 +1,24 @@
+/** Tiposde variavel 
+ *
+ *string 
+ * Number
+ * Boolean
+ * Null
+ * Underfine
+*/
+
+var nome = "davi";
+var numero = 21;
+var letra = 'D';
+var decimal = 1234.1234;
+var logico = true;
+var vnull = null;
+var indefinido;
+
+console.log(nome);
+console.log(numero);
+console.log(letra);
+console.log(decimal);
+console.log(logico);
+console.log(vnull);
+console.log(indefinido);
