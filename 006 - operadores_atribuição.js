453719d4f4e560fe81cbd9046ad1fcb2f66407a6
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-07 17:44:09 -0300

    tdsa

---
+++ 006 - operadores_atribuição.js
@@ -0,0 +1,23 @@
+/*
+   Igual - == - X == 10
+   
+   Diferente - != - X != 10
+
+   Menor que - < - X < 10
+
+   Menor ou igual - <= - X <= 10
+
+   Maior que - > - X > 10
+
+   Maior ou igual - >= - X >= 10
+*/
+
+let valor = 2; // Incremento
+console.log(valor)
+valor += 1
+console.log(valor)
+// Decremento
+let valor2 = 3;
+console.log(valor2)
+valor2 += 1
+console.log(valor2)
