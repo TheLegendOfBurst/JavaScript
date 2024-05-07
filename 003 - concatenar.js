f2dd4d09b5323b874f0f3d55577d05c0b52819f5
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-07 14:24:02 -0300

    003 att

--- 003 - concatenar.js
+++ 003 - concatenar.js
@@ -1,15 +1,23 @@
-/*
+
 var dono = 'Davi';
 var ddd = '(61)';
 var telefone = '9 9999-9999';
 var numero = ddd.concat(" " + telefone);
+
+// Concatenado com ','
 console.log ("O Cliente", dono, "tem o seguinte numero de celular",ddd, telefone);
+
+// Concatenado com '+'
 console.log ("O Cliente "+ dono + " tem o seguinte numero de celular "+ numero);
 
-let texto = `O Cliente ${dono} tem o seguinte numero de celular ${ddd} ${telefone}`
-console.log(texto)
-*/
+// Concatenado em variavel com '$'
+let texto = `O Cliente ${dono} tem o seguinte numero de celular ${ddd} ${telefone}`;
+console.log(texto);
 
+//Idade como Numero
+var idade = 28;
+console.log(idade + 1)
 
-var idade = 21
-console.log(idade + 1);
+//Transforma idade em String
+var idade = 28;
+console.log(idade.toString() + 1);
