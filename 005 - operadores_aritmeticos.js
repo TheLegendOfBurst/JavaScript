2f290561583a2d6518560fe4cce6cdde178570e1
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-07 15:42:37 -0300

    Update 005 - operadores_aritmeticos.js

--- 005 - operadores_aritmeticos.js
+++ 005 - operadores_aritmeticos.js
@@ -19,9 +19,10 @@ console.log("A soma é: "+soma)
 console.log("A soma é:",v1+v2)
 console.log("A soma é: "+parseInt(v1+v2))
 console.log("A soma é: "+Number(v1+v2))
+console.log("")
 
 // Subtração
-let v3 = 10;
+let v3 = 69;
 let v4 = 5;
 let subtracao = v3-v4;
 console.log(v3-v4)
@@ -29,32 +30,67 @@ console.log("A subtração é: "+subtracao)
 console.log("A subtração é:",v3-v4)
 console.log("A subtração é: "+parseInt(v3-v4))
 console.log("A subtração é: "+Number(v3-v4))
+console.log("")
 
 // Divisao
-let v5 = 10;
-let v6 = 5;
+let v5 = 27;
+let v6 = 75;
 let divisao = v5/v6;
 console.log(v5/v6)
 console.log("A divisão é: "+divisao)
 console.log("A divisão é:",v5/v6)
 console.log("A divisão é: "+parseInt(v5/v6))
 console.log("A divisão é: "+Number(v5/v6))
+console.log("")
 
 // Divisao
-let v7 = 10;
-let v8 = 5;
+let v7 = 32;
+let v8 = 81;
 let multipicacao = v7*v8;
 console.log(v7*v8)
 console.log("A multiplicação é: "+multipicacao)
 console.log("A multiplicação é:",v7*v8)
 console.log("A multiplicação é: "+parseInt(v7*v8))
 console.log("A multiplicação é: "+Number(v7*v8))
+console.log("")
 
 // RestoDivisão
-let v9 = 10;
-let v10 = 5;
+let v9 = 65;
+let v10 = 92;
 let restodivisao = v9 % v10;
 console.log("O restodivisão é: " + restodivisao);
 console.log("O restodivisão é:", restodivisao);
 console.log("O restodivisão é: " + parseInt(restodivisao));
 console.log("O restodivisão é: " + Number(restodivisao));
+console.log("")
+
+// Incremento
+let v11 = 65;
+let v12 = 92;
+let incremento = v11 + v12;
+console.log("O incremento é: " + incremento);
+console.log("O incremento é:", incremento);
+console.log("O incremento é: " + parseInt(incremento));
+console.log("O incremento é: " + Number(incremento));
+console.log("");
+
+// Decremento
+let v13 = 743;
+let v14 = 742;
+let decremento = v13 - v14;
+console.log("O decremento é: " + decremento);
+console.log("O decremento é:", decremento);
+console.log("O decremento é: " + parseInt(decremento));
+console.log("O decremento é: " + Number(decremento));
+console.log("");
+
+// Exponenciamento
+let v15 = 157;
+let v16 = 751;
+let exponenciamento = v15 ** v16;
+console.log("O exponenciamento é: " + exponenciamento);
+console.log("O exponenciamento é:", exponenciamento);
+console.log("O exponenciamento é: " + parseInt(exponenciamento));
+console.log("O exponenciamento é: " + Number(exponenciamento));
+console.log("");
+
