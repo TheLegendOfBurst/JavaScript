57debade03627cabc710e91055295ac7d237837b
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-07 14:27:19 -0300

    Update 005 - operadores_aritmeticos.js

--- 005 - operadores_aritmeticos.js
+++ 005 - operadores_aritmeticos.js
@@ -10,40 +10,42 @@ TIPOS
     Expoente(**)
 */
 
-let v1 = 2;
-let v2 = 3;
+// Adição
+let v1 = 10;
+let v2 = 15;
 let soma = v1+v2;
-console.log("A soma é:"+soma)
+console.log(v1+v2)
+console.log("A soma é: "+soma)
 console.log("A soma é:",v1+v2)
-console.log("A soma é:"+parseInt(v1+v2))
-console.log("A soma é:"+Number(v1+v2))
+console.log("A soma é: "+parseInt(v1+v2))
+console.log("A soma é: "+Number(v1+v2))
 
-//SUBTRAÇÃO
+// Subtração
+let v3 = 10;
+let v4 = 5;
+let subtracao = v3-v4;
+console.log(v3-v4)
+console.log("A subtração é: "+subtracao)
+console.log("A subtração é:",v3-v4)
+console.log("A subtração é: "+parseInt(v3-v4))
+console.log("A subtração é: "+Number(v3-v4))
 
-let v3 = 9;
-let v4 = 4;
-let subtracao = v3+v4;
-console.log("A subtração é:"+subtracao)
-console.log("A subtração é:",v3+v4)
-console.log("A subtração é:"+parseInt(v3+v4))
-console.log("A subtração é:"+Number(v3+v4))
-
-//DIVISÃO
-
+// Divisao
 let v5 = 10;
-let v6 = 15;
-let divisao = v5+v6;
-console.log("A divisão é:"+divisao)
-console.log("A divisão é:",v5+v6)
-console.log("A divisão é:"+parseInt(v5+v6))
-console.log("A divisão é:"+Number(v5+v6))
+let v6 = 5;
+let divisao = v5/v6;
+console.log(v5/v6)
+console.log("A divisão é: "+divisao)
+console.log("A divisão é:",v5/v6)
+console.log("A divisão é: "+parseInt(v5/v6))
+console.log("A divisão é: "+Number(v5/v6))
 
-//MULTIPLICAÇÃO
-
+// Divisao
 let v7 = 10;
-let v8 = 15;
-let multiplicacao = v7+v8;
-console.log("A multiplicação é:"+multiplicacao)
-console.log("A multiplicação é:",v7+v8)
-console.log("A multiplicação é:"+parseInt(v7+v8))
-console.log("A multiplicação é:"+Number(v7+v8))
+let v8 = 5;
+let multipicacao = v7*v8;
+console.log(v7*v8)
+console.log("A multiplicação é: "+multipicacao)
+console.log("A multiplicação é:",v7*v8)
+console.log("A multiplicação é: "+parseInt(v7*v8))
+console.log("A multiplicação é: "+Number(v7*v8))
