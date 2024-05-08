8300221696da5c77eb30ccf56868d07df2e8645b
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-08 17:46:21 -0300

    af

---
+++ 007 - operadores_relacionais.js
@@ -0,0 +1,51 @@
+/*
+    Igual (=)
+    Diferente (!=)
+    Menor que (<)
+    Menor ou igual (<=)
+    Maior que (>)
+    Maior ou igual (>=)
+*/
+
+//if: SE
+//else: SENÃO
+
+
+let a = 5;
+let b = 7;
+
+if (a == b) {
+    console.log("a é igual a b");
+} else {
+    console.log("a não é igual a b");
+}
+
+if (a != b) {
+    console.log("a é diferente de b");
+} else {
+    console.log("a não é diferente de b");
+}
+
+if (a < b) {
+    console.log("a é menor que b");
+} else {
+    console.log("a não é menor que b");
+}
+
+if (a <= b) {
+    console.log("a é menor ou igual a b");
+} else {
+    console.log("a não é menor ou igual a b");
+}
+
+if (a > b) {
+    console.log("a é maior que b");
+} else {
+    console.log("a não é maior que b");
+}
+
+if (a >= b) {
+    console.log("a é maior ou igual a b");
+} else {
+    console.log("a não é maior ou igual a b");
+}
---
+++ 008 - operadores_logicos.js
@@ -0,0 +1,6 @@
+/*
+OPERADORES LOGICOS
+E (AND) - &&(java script) - ((x<10) && (x>0))
+OU (OR) - ||(java script) - ((x<10) || (x>0))
+NEGAÇÃO - !(java script) - (!true = false) 
+*/
