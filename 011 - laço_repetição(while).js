f1c0fe7dc1192a76577a08836612457781fc8bda
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-09 17:39:07 -0300

    Update 011 - laço_repetição(while).js

--- 011 - laço_repetição(while).js
+++ 011 - laço_repetição(while).js
@@ -7,3 +7,45 @@
     quando precisamos repetir uma ação até que uma condição 
     específica seja atendida.
 */
+
+//contagem regressiva usando while
+let contador = 5;
+while (contador >= 1) {
+  console.log(contador);
+  contador--;
+}
+//Gerar números aleatórios até atingir um limite
+let numero;
+do {
+  numero = parseInt(prompt("Digite um número maior que 10:"));
+} while (numero <= 10);
+
+console.log("Número válido: " + numero);
+
+let numeroAleatorio;
+while (true) {
+  numeroAleatorio = Math.random(); // Gera um número aleatório entre 0 e 1
+  console.log(numeroAleatorio);
+  if (numeroAleatorio > 0.8) {
+    break; // Sai do loop se o número gerado for maior que 0.8
+  }
+}
+//Exemplo 2: Contagem de tentativas de login
+let senhaCorreta = "senha123";
+let tentativas = 3;
+
+while (tentativas > 0) {
+  let senhaUsuario = prompt("Digite sua senha:");
+  if (senhaUsuario === senhaCorreta) {
+    console.log("Login bem-sucedido!");
+    break; // Sai do loop se a senha estiver correta
+  } else {
+    tentativas--;
+    console.log("Senha incorreta. Tentativas restantes: " + tentativas);
+  }
+}
+
+if (tentativas === 0) {
+  console.log("Você excedeu o número de tentativas permitidas. Contate o suporte.");
+}
+
