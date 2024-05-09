7ee3e226c4782d729fc689d8853a6b2022d64e88
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-09 15:03:22 -0300

    Update 009 - arrays.js

--- 009 - arrays.js
+++ 009 - arrays.js
@@ -8,23 +8,39 @@ são indexados numericamente, começando do índice 0,
  específica no array.
 */
 
-let array1 = [1,2,3,4,5]
+// Usando colchetes []
+let array1 = [1, 2, 3, 4, 5];
 
+// Usando o construtor Array()
+let array2 = new Array(1, 2, 3, 4, 5);
 
-let array2 = new Array(1,2,3,4,5)
-
+// Criando um array vazio []
 let array3 = [];
 
-let array4 = ["Hello",42, true];
+// Array podem conter diferentes tipos de dados
+let array4 = ["Hello", 42, true];
 
+// Acessando indice do Array
 let array = [10, 20, 30, 40, 50];
-console.log(array[0])
-console.log(array[2])
+console.log(array[0])// Saida 10
+console.log(array[2])// Saida 30
 
+// Verificando o tamanho do array
 let arraylength = [1,2,3]
-console.log(arraylength, length);
+console.log(arraylength.length); // Saida 3
 
+// Inserindo um item no array
 arraylength.push(4)
+console.log(arraylength) // Saida [1, 2, 3, 4]
+
+// Removendo um item no array
+let removedElement = arraylength.pop();
+console.log(removedElement)// Removendo o 4
 console.log(arraylength)
 
-let removedElement = arraylength
+// Removendo um item pelo index no array
+let array5 = [1, 2, 3, 4, 5];
+// array5.splice(index, deleteNumbers)
+array5.splice(2, 1)
+console.log(array5)// Saida: [1, 2, 4, 5]
+console.log(arraylength)
