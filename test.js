f86bdaf362fe67935843e87380e68c45b5fa33d8
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-05-13 14:45:10 -0300

    tat

---
+++ test.js
@@ -0,0 +1,4 @@
+setInterval(function() {
+    // Código da função que será executada a cada 3 segundos
+    console.log("Função executada a cada 3 segundos.");
+}, 3000); // 3000 milissegundos = 3 segundos
